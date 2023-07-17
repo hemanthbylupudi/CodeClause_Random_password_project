3b50b407e236994943674fab464c9305d2f34cc3
hemanthbylupudi <122376842+hemanthbylupudi@users.noreply.github.com>
2023-07-17 13:34:19 +0530

    Update randompass.py

--- randompass.py
+++ randompass.py
@@ -1,6 +1,6 @@
 import random
 import string
-def get_random_password(length=8):
+def get_random_password(length=8):  # it genarates the password with the length 8 if no arguments will given
     characters = string.ascii_letters + string.digits + string.punctuation
     password = ""
     for i in range(length):
